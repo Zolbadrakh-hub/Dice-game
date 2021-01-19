bdc833e22289b177cb0260419ce9632d0980b2c8
Zolbadrakh <rainwons@gmail.com>
2021-01-19 23:11:02 +0800

    Modified - 2

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ var scores = [0, 0];
 var roundscore = 0;
 //Шооны аль талаар буусан хувьсагчийг оноог хадгалах хэрэгтэй, 1-6 хүртэл санамсаргүй сонгох хэрэгтэй.
 
-var diceNumber = Math.floor(Math.random() * 6) + 1;
 
-document.getElementById('#score-0').textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+
+document.getElementById('score-0').textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener('click', function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+})
