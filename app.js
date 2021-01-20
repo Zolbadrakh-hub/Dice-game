a6bec16804280baa71e624b18dfb444ccc4b233f
Zolbadrakh <rainwons@gmail.com>
2021-01-20 16:39:31 +0800

    onoog oorchilj current deer nemne

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 // Тоглогчдын оноог цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 //  Тоглогчийн ээлжийн оноог цуглуулах хувьсагч
-var roundscore = 0;
+var roundScore = 0;
 //Шооны аль талаар буусан хувьсагчийг оноог хадгалах хэрэгтэй, 1-6 хүртэл санамсаргүй сонгох хэрэгтэй.
 
 
@@ -15,8 +15,31 @@ document.getElementById("current-1").textContent = 0;
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+//Шоог шидэх евент листентэр
 document.querySelector(".btn-roll").addEventListener('click', function(){
+    // 1 - 6 гийн хооронд тоог санамсаргүй тоог гаргах
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    //Шоог гаргаж ирэх
     diceDom.style.display = "block";
+    // Санамсаргүй гаргасан тоог зураг болгон өөрчлөх
     diceDom.src = "dice-" + diceNumber + ".png";
+    //Буусан тоо нэгээс ялгаатай бол идэвхтэй тоглогчийн оноог өөрчлөнө.
+    //Тоглогчийн ээлжийн оноог өөрчлөнө.
+    if(diceNumber !== 1){
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    }
+    else
+    {
+        roundScore = 0;
+        document.getElementById("current-" +activePlayer).textContent = 0;
+
+       activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+       document.querySelector(".player-0-panel").classList.toggle("active");
+       document.querySelector(".player-1-panel").classList.toggle("active");
+       diceDom.style.display = " none";
+
+    }
 })
+
